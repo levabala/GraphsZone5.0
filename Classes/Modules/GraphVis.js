ef1254c94dc4f9bb126d98ca27418c35406e4f1a
levabala <levabala@ya.ru>
2016-07-03 14:10:37 +0300

    v0.6.2

--- Classes/Modules/GraphVis.js
+++ Classes/Modules/GraphVis.js
@@ -6,15 +6,17 @@ function GraphVis(id, field, domId) {
     var data = new vis.DataSet();
     // create some nice looking data with sin/cos
     var counter = 0;
-    var steps = 50;  // number of datapoints will be steps*steps
-    var axisMax = 314;
-    var axisStep = axisMax / steps;
-    for (var x = 0; x < axisMax; x += axisStep) {
-        for (var y = 0; y < axisMax; y += axisStep) {
-            var value = (Math.sin(x / 50) * Math.cos(y / 50) * 50 + 50);
+    var steps = 40;  // number of datapoints will be steps*steps
+    var axisMaxX = 200;
+    var axisMaxY = 100;
+    var axisStepX = axisMaxX / steps;
+    var axisStepY = axisMaxY / steps;
+    for (var x = 0; x < axisMaxX; x += axisStepX) {
+        for (var y = 0; y < axisMaxY; y += axisStepY) {
+            var value = (Math.sin(x / 50) * Math.cos(y / 50) * 50 + 50);            
             data.add({ id: counter++, x: x, y: y, z: value, style: value });
         }
-    }
+    }    
 
     // specify options
     var options = {
@@ -39,17 +41,29 @@ function GraphVis(id, field, domId) {
         distance: 1.7
     });
 
-    this.dom.insertBefore(this.mb,this.dom.firstChild);
+    this.dom.insertBefore(this.mb, this.dom.firstChild);
 
     console.log(graph3d)
 
     this.plot = graphDom;
     this.blockDragging();
 
-    this.dom.onchange = function () {        
+    this.dom.onchange = function () {
         graph3d.setSize(grv.dom.width, grv.dom.height);
         graphDom.style.width = grv.dom.width + 'px';
         graphDom.style.height = grv.dom.height + 'px';
-        graph3d.redraw();        
+        graph3d.redraw();
     };
+
+    //Random points
+    /*RandomPoints.postMessage('get');        
+    RandomPoints.onmessage = function (arr) {
+        console.log('Rendered')
+        gPl.dom.innerHTML = "";
+        Plotly.plot(gPl.dom, arr.data, layout);            
+        plot.onmousedown = function (event) {
+            event.stopPropagation();
+        };
+    };*/
+
 }
